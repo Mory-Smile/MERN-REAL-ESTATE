2acf79d7b8eca450a6cb1c607f050c0bc7579c16
Mory <Mory@gmail.com>
2025-10-21 13:00:22 +0300

    Created test api route and controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,13 @@ mongoose
 
 const app = express();
 
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello World!" });
+//   // res.send("Hello World!");
+// });
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRouter);
