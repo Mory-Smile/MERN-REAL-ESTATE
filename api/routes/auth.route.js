a3fe9bad19e7659a341fbda9b53b2fb7575c886c
Mory <Mory@gmail.com>
2025-10-26 21:02:17 +0300

    Created sign out API successfully

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { google, signup, signin } from "../controllers/auth.controller.js";
+import {
+  google,
+  signup,
+  signin,
+  signout,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
 router.post("/sign-in", signin);
 router.post("/google", google);
+router.get("/sign-out", signout);
 
 export default router;
