86c2e321e6ff6aec9052d8019c4ab45d2b9af68a
Mory <Mory@gmail.com>
2025-10-30 12:03:19 +0300

    Created get user listings api route successfully

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const updateUser = async (req, res) => {
@@ -33,5 +34,16 @@ export const deleteUser = async (req, res, next) => {
       .json("User has been deleted successfully!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.user.params) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {}
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
