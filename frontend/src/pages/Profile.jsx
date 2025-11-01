428faf2838d35ab2d5524ea2a5cdfb99e036c45f
Mory <Mory@gmail.com>
2025-11-01 12:48:32 +0200

    Finished delete listing logic and created update listing api and tested it successfully

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -149,6 +149,25 @@ const Profile = () => {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -244,7 +263,12 @@ const Profile = () => {
               </Link>
 
               <div className="flex flex-col">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
